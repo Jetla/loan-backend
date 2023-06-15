2978d10d32b5b7983b43e1779315dda7dc9715b6
Sivannarayana Buddala <sivanarayan19@gmail.com>
2023-06-15 09:56:28 +0530

    depedencies added

--- lmsBackend/src/main/java/com/sample/LoanManagementApplication.java
+++ lmsBackend/src/main/java/com/sample/LoanManagementApplication.java
@@ -4,14 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 //@SpringBootApplication
+
 @EnableAutoConfiguration(exclude = {
 		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class })
 @EntityScan(basePackages="com.sample.Model")
 @EnableJpaRepositories(basePackages="com.sample.Repository")
-@SpringBootApplication(scanBasePackages="com")
+@SpringBootApplication(scanBasePackages="com",exclude = {DataSourceAutoConfiguration.class })
 public class LoanManagementApplication {
 
 	public static void main(String[] args) {
